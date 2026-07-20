94ebe6c927e608f23f1a401c5eb2ed72bd311fe4
kyj7479 <kyj7479@users.noreply.github.com>
2026-07-21 00:03:41 +0900

    refactor: 스타일, 환경변수 등 분리

---
+++ wedding.config.js
@@ -0,0 +1,23 @@
+/**
+ * 모바일 청첩장 기본 정보
+ * 이 파일의 값만 바꾸면 페이지 전반에 자동으로 반영됩니다.
+ */
+window.WEDDING_CONFIG = {
+  groomLastName: "김",
+  groomFirstName: "영진",
+  brideLastName: "장",
+  brideFirstName: "예슬",
+  date: "2026-11-22T12:20:00+09:00",
+  venueName: "스타시티아트홀",
+  hallName: "",
+  address: "서울특별시 광진구 화양동 능동로 110 스타시티영존 5층",
+  contactPhone: "010-0000-0000",
+  heroPhoto: "./photos/PYD00070.jpg",
+  heroPhotoAlt: "김영진과 장예슬의 웨딩 사진",
+  heroLabel: "WE ARE GETTING MARRIED",
+  heroMessage: "서로의 가장 빛나는 순간을\n함께해 주시면 감사하겠습니다.",
+  invitationTitle: "소중한 분들을 초대합니다",
+  invitationMessage: "오랜 시간 서로를 바라보며 걸어온 두 사람이\n이제 같은 길을 함께 걸어가려 합니다.\n따뜻한 마음으로 축복해 주시면 감사하겠습니다.",
+  locationButton: "오시는 길",
+  contactButton: "마음 전하기",
+};
