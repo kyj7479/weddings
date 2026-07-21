29d43f4887dcc78de65e6c25102c0f4d3594cac5
kyj7479 <kyj7479@users.noreply.github.com>
2026-07-21 21:57:52 +0900

    Center family information layout

--- sections/family.js
+++ sections/family.js
@@ -5,14 +5,14 @@ export default function createFamily(content) {
     <div class="family-rule"></div>
     <div class="family-list">
       <p class="family-row">
-        <span class="parents">${content.groomParents.father} · ${content.groomParents.mother}</span>
-        <span class="relationship">의 아들</span>
+        <span class="parents">${content.groomParents.father} · ${content.groomParents.mother}의</span>
+        <span class="relationship">아들</span>
         <span class="role">신랑</span>
         <span class="name">${content.groomName}</span>
       </p>
       <p class="family-row">
-        <span class="parents">${content.brideParents.father} · ${content.brideParents.mother}</span>
-        <span class="relationship">의 딸</span>
+        <span class="parents">${content.brideParents.father} · ${content.brideParents.mother}의</span>
+        <span class="relationship">딸</span>
         <span class="role">신부</span>
         <span class="name">${content.brideName}</span>
       </p>
